e96bbf08c13d6d6194a805bd9ccbe898f2a146d2
Puy0zz <111022121+Puy0zz@users.noreply.github.com>
2024-05-14 08:47:31 -0500

    Update p6_4.py

--- p6_4.py
+++ p6_4.py
@@ -5,13 +5,8 @@ Created on Tue Mar 26 20:39:56 2024
 @author: Juan
 """
 
-import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
 import numpy as np
-from scipy import interpolate as inter
-from scipy import stats as st
-from puystats import linregress_w as linr
+
 from scipy.optimize import minimize as mini
 from scipy.optimize import newton as nwt
 
@@ -54,7 +49,7 @@ def f_c(c,m=m_min,x=x,y=y,alpha=dy,contor=1):
 Metodo para incertidumbre de la pendiente m
 """
 
-def Met_iter_m(m=m_min,c=c_min,x=x,y=y,alpha=dy,gmm=0.05,err=1e-7,contor=1):
+def Met_iter_m(m=m_min,c=c_min,x=x,y=y,alpha=dy,gmm=0.05,err=1e-7,contor=1,signo=-1):
     
     cj_1=c
     d=1
@@ -64,11 +59,11 @@ def Met_iter_m(m=m_min,c=c_min,x=x,y=y,alpha=dy,gmm=0.05,err=1e-7,contor=1):
     while d>err:
         mj_1=mj
         """
-        Si se quiere que vaya a la derecha cambiar signo del 0.02 en el segundo argumento
-        de la función nwt() de la línea de abajo por un +.
-        Si en cambio se quiere ir a la izquierda cambiar el signo de la línea de abjo por -
+        Si se quiere que vaya a la derecha cambiar la variable signo del 0.02 en el segundo argumento
+        de la función nwt() de la línea de abajo por un +1.
+        Si en cambio se quiere ir a la izquierda cambiar la variable signo de la línea de abjo por -1
         """
-        mj=nwt(f_m,mj-0.02,args=(cj_1,x,y,alpha,contor))
+        mj=nwt(f_m,mj+signo*0.02,args=(cj_1,x,y,alpha,contor))
         D_cj=2*gmm*np.sum((y-(mj)*x-cj_1)/(alpha)**2)
         
         
@@ -84,7 +79,7 @@ def Met_iter_m(m=m_min,c=c_min,x=x,y=y,alpha=dy,gmm=0.05,err=1e-7,contor=1):
 """
 Metodo para incertidumbre del intercepto c
 """
-def Met_iter_c(m=m_min,c=c_min,x=x,y=y,alpha=dy,gmm=0.0001,err=1e-7,contor=1):
+def Met_iter_c(m=m_min,c=c_min,x=x,y=y,alpha=dy,gmm=0.0001,err=1e-7,contor=1,signo=-1):
     
     mj_1=m
     d=1
@@ -94,11 +89,11 @@ def Met_iter_c(m=m_min,c=c_min,x=x,y=y,alpha=dy,gmm=0.0001,err=1e-7,contor=1):
     while d>err:
         cj_1=cj
         """
-        Si se quiere que vaya a la derecha cambiar signo del 0.02 en el segundo argumento
-        de la función nwt() de la línea de abajo por un +.
-        Si en cambio se quiere ir a la izquierda cambiar el signo de la línea de abjo por -
+        Si se quiere que vaya a la derecha cambiar la variable signo del 0.02 en el segundo argumento
+        de la función nwt() de la línea de abajo por un +1.
+        Si en cambio se quiere ir a la izquierda cambiar la variable signo de la línea de abjo por -1
         """
-        cj=nwt(f_c,cj-0.01,args=(mj_1,x,y,alpha,contor))
+        cj=nwt(f_c,cj+signo*0.01,args=(mj_1,x,y,alpha,contor))
         D_mj=2*gmm*np.sum(x*(y-(mj_1)*x-cj)/(alpha)**2)
         
         
@@ -116,12 +111,71 @@ Para evaluar los distintos valores que se piden en el problema ya sea
 para el contorno de Delta X^2=1,4 o 9, cambiar el argumento contor. Esta puesto
 en 9 ya que ese fue el último que se evaluó
 """
-alpha_m,iteracion= Met_iter_m(contor=9)
-print(alpha_m,iteracion)
-print((alpha_m-m_min)/3)
-alpha_c,iteracion_c= Met_iter_c(contor=9)
-print(alpha_c,iteracion_c)
-print((alpha_c-c_min)/3)
+"""
+contorno Delta X^2=1
+"""
+print("************************")
+print("contorno Delta X^2=1, incrementando")
+alpha_m,iteracion= Met_iter_m(contor=1,signo=1)
+print("m'1+': ",alpha_m)
+print("alpha_m'1+: ",(alpha_m-m_min))
+alpha_c,iteracion_c= Met_iter_c(contor=1,signo=1)
+print("c'_1+",alpha_c)
+print("alpha_c'1+",(alpha_c-c_min))
+print("************************")
+print("************************")
+print("contorno Delta X^2=1, disminuyendo")
+alpha_m,iteracion= Met_iter_m(contor=1,signo=-1)
+print("m'1-': ",alpha_m)
+print("alpha_m'1-: ",(alpha_m-m_min))
+alpha_c,iteracion_c= Met_iter_c(contor=1,signo=-1)
+print("c'_1-",alpha_c)
+print("alpha_c'1-",(alpha_c-c_min))
+print("************************")
+
+"""
+contorno Delta X^2=4
+"""
+print("************************")
+print("contorno Delta X^2=4, incrementando")
+alpha_m,iteracion= Met_iter_m(contor=4,signo=1)
+print("m'4+': ",alpha_m)
+print("alpha_m'4+: ",(alpha_m-m_min)/2)
+alpha_c,iteracion_c= Met_iter_c(contor=4,signo=1)
+print("c'_4+",alpha_c)
+print("alpha_c'4+",(alpha_c-c_min)/2)
+print("************************")
+print("************************")
+print("contorno Delta X^2=4, disminuyendo")
+alpha_m,iteracion= Met_iter_m(contor=4,signo=-1)
+print("m'4-': ",alpha_m)
+print("alpha_m'4-: ",(alpha_m-m_min)/2)
+alpha_c,iteracion_c= Met_iter_c(contor=4,signo=-1)
+print("c'_4-",alpha_c)
+print("alpha_c'4-",(alpha_c-c_min)/2)
+print("************************")
+
+"""
+contorno Delta X^2=9
+"""
+print("************************")
+print("contorno Delta X^2=9, incrementando")
+alpha_m,iteracion= Met_iter_m(contor=9,signo=1)
+print("m'9+': ",alpha_m)
+print("alpha_m'9+: ",(alpha_m-m_min)/3)
+alpha_c,iteracion_c= Met_iter_c(contor=9,signo=1)
+print("c'_9+",alpha_c)
+print("alpha_c'9+",(alpha_c-c_min)/3)
+print("************************")
+print("************************")
+print("contorno Delta X^2=9, disminuyendo")
+alpha_m,iteracion= Met_iter_m(contor=9,signo=-1)
+print("m'9-': ",alpha_m)
+print("alpha_m'9-: ",(alpha_m-m_min)/3)
+alpha_c,iteracion_c= Met_iter_c(contor=9,signo=-1)
+print("c'_9-",alpha_c)
+print("alpha_c'9-",(alpha_c-c_min)/3)
+print("************************")
 
 
 
